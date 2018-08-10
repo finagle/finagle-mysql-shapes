34a5b668a4fe5eadb6d3e4755d05d09745318b52
Alex Moreno <alex.moreno@linecorp.com>
2018-08-10 16:01:17 +0800

    add value decoders for most things

--- src/main/scala/com/linecorp/falcon/mysql/ValueDecoder.scala
+++ src/main/scala/com/linecorp/falcon/mysql/ValueDecoder.scala
@@ -31,32 +31,76 @@ object ValueDecoder {
     }
   }
 
-  implicit val decodeInt: ValueDecoder[Int] = new ValueDecoder[Int] {
-    def from(value: Value): Try[Int] = value match {
-      case IntValue(x) => Success(x)
-      case _           => fail("failed to read int")
-    }
+  implicit val decodeByte: ValueDecoder[Byte] = ValueDecoder.instance {
+    case ByteValue(b) => Success(b)
+    case _ => fail("failed to decode Byte")
   }
 
-  implicit val decodeString: ValueDecoder[String] = new ValueDecoder[String] {
-    def from(value: Value): Try[String] = value match {
-      case StringValue(x) => Success(x)
-      case _              => fail("failed to read int")
-    }
+  implicit val decodeShort: ValueDecoder[Short] = ValueDecoder.instance {
+    case ShortValue(s) => Success(s)
+    case _ => fail("failed to decode Short")
+  }
+
+  implicit val decodeInt: ValueDecoder[Int] = ValueDecoder.instance {
+    case IntValue(i) => Success(i)
+    case _ => fail("failed to decode Int")
+  }
+
+  implicit val decodeLong: ValueDecoder[Long] = ValueDecoder.instance {
+    case LongValue(l) => Success(l)
+    case _ => fail("failed to decode Long")
+  }
+
+  implicit val decodeBigInt: ValueDecoder[BigInt] = ValueDecoder.instance {
+    case BigIntValue(bi) => Success(bi)
+    case _ => fail("failed to decode BigInt")
+  }
+
+  implicit val decodeFloat: ValueDecoder[Float] = ValueDecoder.instance {
+    case FloatValue(f) => Success(f)
+    case _ => fail("failed to decode Float")
+  }
+
+  implicit val decodeDouble: ValueDecoder[Double] = ValueDecoder.instance {
+    case DoubleValue(d) => Success(d)
+    case _ => fail("failed to decode Double")
+  }
+
+  implicit val decodeString: ValueDecoder[String] = ValueDecoder.instance {
+    case StringValue(s) => Success(s)
+    case _ => fail("failed to decode String")
   }
 
   implicit val decodeDate: ValueDecoder[Date] = ValueDecoder.instance {
     DateValue.unapply(_) match {
       case Some(date) => Success(date)
-      case None       => ValueDecoder.fail("failed to read java.sql.Date")
+      case None => ValueDecoder.fail("failed to decode java.sql.Date")
     }
   }
 
   implicit val decodeTimestamp: ValueDecoder[Timestamp] = ValueDecoder.instance {
     TimestampValue.unapply(_) match {
       case Some(date) => Success(date)
-      case None       => ValueDecoder.fail("failed to read java.sql.Timestamp")
+      case None => ValueDecoder.fail("failed to decode java.sql.Timestamp")
     }
+  }
+
+  implicit val decodeBigDecimal: ValueDecoder[BigDecimal] = ValueDecoder.instance {
+    BigDecimalValue.unapply(_) match {
+      case Some(b) => Success(b)
+      case None => ValueDecoder.fail("failed to decode java.sql.BigDecimal")
+    }
+  }
+
+  // Binary
+
+  private def isBinary(`type`: Short) =
+    Type.TinyBlob to Type.String contains `type`
+
+  implicit val decodeBinary: ValueDecoder[Array[Byte]] = ValueDecoder.instance {
+    case RawValue(typ, _, _, bytes) if isBinary(typ) => Success(bytes)
+    case EmptyValue => Success(Array.emptyByteArray)
+    case _ => fail("failed to decode binary blob")
   }
 
   // Option
@@ -65,7 +109,7 @@ object ValueDecoder {
     new ValueDecoder[Option[T]] {
       def from(value: Value): Try[Option[T]] = value match {
         case NullValue => Success(None)
-        case _         => ValueDecoder[T].from(value).map(Some(_))
+        case _ => ValueDecoder[T].from(value).map(Some(_))
       }
     }
 
