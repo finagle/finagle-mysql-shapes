3ac8687d735052e93027b172b75bef5c52cd1935
Alex Moreno <alex.moreno@linecorp.com>
2018-12-11 11:17:26 +0800

    update api documentation

--- src/main/scala/com/linecorp/falcon/mysql/syntax/package.scala
+++ src/main/scala/com/linecorp/falcon/mysql/syntax/package.scala
@@ -5,15 +5,25 @@ import com.twitter.finagle.mysql._
 import shapeless._
 
 package object syntax {
-
+  /**
+    * Enriches
+    * [[https://twitter.github.io/finagle/docs/com/twitter/finagle/mysql/Row.html com.twitter.finagle.mysql.Row]]
+    * instances with methods to decode to a specific type.
+    */
   implicit class RichRow(val row: Row) extends AnyVal {
 
+    /**
+      *  Decode a column into the specified type.
+      */
     def get[A: ValueDecoder](column: String): Try[A] =
       row.apply(column) match {
         case Some(value) => ValueDecoder[A].from(value)
         case None        => ValueDecoder.fail(s"column not found")
       }
 
+    /**
+      *  Decode a row into the specified type.
+      */
     def as[A: RowDecoder]: Try[A] = RowDecoder[A].from(row)
 
   }
